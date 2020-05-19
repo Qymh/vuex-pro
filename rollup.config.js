77299003f43c224033097d18674e9d21467187fc
Qymh <415570450@qq.com>
2020-05-19 15:45:06 +0800

    feat: build

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,11 @@ import replace from '@rollup/plugin-replace';
 import livereload from 'rollup-plugin-livereload';
 import serve from 'rollup-plugin-serve';
 import htmlTemplate from 'rollup-plugin-generate-html-template';
+import { terser } from 'rollup-plugin-terser';
 
 const target = process.env.TARGET || 'pkg';
+const env =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 const config = {
   pkg: {
@@ -15,8 +18,57 @@ const config = {
       file: './dist/vuex-pro-dev.js',
       format: 'umd',
       name: 'Vuex'
-    }
+    },
+    plugins: [
+      ts({
+        check: false,
+        tsconfigOverride: { compilerOptions: { declaration: true } }
+      })
+    ]
   },
+  pkgProd: [
+    {
+      input: 'src/index.ts',
+      output: {
+        file: './dist/vuex-pro-esm.js',
+        format: 'esm'
+      },
+      external: ['vue'],
+      plugins: [
+        ts({
+          check: false,
+          tsconfigOverride: { compilerOptions: { declaration: true } }
+        })
+      ]
+    },
+    {
+      input: 'src/index.ts',
+      output: {
+        file: './dist/vuex-pro-esm.min.js',
+        format: 'esm'
+      },
+      external: ['vue'],
+      plugins: [ts({ check: false }), terser()]
+    },
+    {
+      input: 'src/index.ts',
+      output: {
+        file: './dist/vuex-pro.js',
+        format: 'umd',
+        name: 'Vuex'
+      },
+      plugins: [ts({ check: false })]
+    },
+    {
+      input: 'src/index.ts',
+      output: {
+        file: './dist/vuex-pro.min.js',
+        format: 'umd',
+        name: 'Vuex'
+      },
+      plugins: [ts({ check: false }), terser()]
+    }
+  ],
   demo: {
     input: 'demo/index.ts',
     output: {
@@ -25,6 +77,7 @@ const config = {
       name: 'demo'
     },
     plugins: [
+      ts({ check: false }),
       serve({
         contentBase: ['demoDist'],
         port: 10003
@@ -39,25 +92,33 @@ const config = {
 };
 
 function createConfig(target) {
-  const { input, output, plugins } = config[target];
-  return {
-    input,
-    output,
-    plugins: [
-      ...(plugins || []),
-      cjs(),
-      rs({
-        extensions: ['.js', '.ts']
-      }),
-      ts({
-        check: false
-      }),
-      replace({
-        'process.env.NODE_ENV': '"development"',
-        __DEV__: process.env.NODE_ENV !== 'production'
-      })
-    ]
-  };
+  const defaultPlugins = [
+    cjs(),
+    rs({
+      extensions: ['.js', '.ts']
+    }),
+    replace({
+      'process.env.NODE_ENV': `"${env}"`,
+      __DEV__: env === 'development'
+    })
+  ];
+  if (env === 'production' && target === 'pkg') {
+    target = 'pkgProd';
+  }
+  const curConfig = config[target];
+  if (Array.isArray(curConfig)) {
+    return curConfig.map((v) => {
+      v.plugins = [...defaultPlugins, ...(v.plugins || [])];
+      return v;
+    });
+  } else {
+    const { input, output, plugins } = curConfig;
+    return {
+      input,
+      output,
+      plugins: [...defaultPlugins, ...(plugins || [])]
+    };
+  }
 }
 
 export default createConfig(target);
